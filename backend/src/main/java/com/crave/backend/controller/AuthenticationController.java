9be476524b14e91e2852b509fdb81d974618a80c
Jared Castillo <59758243+jablue-12@users.noreply.github.com>
2023-11-05 21:39:53 -0600

    Retrieve authenticated user
    - Fix bug with not being able to do HTTP request on Accounts. The fix is to add `@JsonIgnore on the tokens attribute to avoid infinite loops and circular references.
    - Create a new endpoint for retrieving the user (auth/profile). You should provide the bearer token in the header.
    - Update Registration to include the USER_ROLE when registering.

--- backend/src/main/java/com/crave/backend/controller/AuthenticationController.java
+++ backend/src/main/java/com/crave/backend/controller/AuthenticationController.java
@@ -1,11 +1,15 @@
 package com.crave.backend.controller;
 
+import com.crave.backend.model.Account;
 import com.crave.backend.model.auth.AuthenticationRequest;
 import com.crave.backend.model.auth.AuthenticationResponse;
 import com.crave.backend.model.auth.RegisterRequest;
+import com.crave.backend.service.AccountService;
 import com.crave.backend.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+    private final AccountService accountService;
 
     @PostMapping(path = "/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
@@ -33,5 +38,16 @@ public class AuthenticationController {
         } else {
             return ResponseEntity.badRequest().body(response);
         }
+    }
+
+    @GetMapping("/profile")
+    @ResponseBody
+    public ResponseEntity<?> viewUserProfile(@AuthenticationPrincipal UserDetails userDetails) {
+        if (userDetails != null) {
+            // userDetails contains information about the authenticated user
+            Account user = accountService.findByEmail(userDetails.getUsername()).orElse(null);
+            return ResponseEntity.ok(user);
+        }
+        return ResponseEntity.badRequest().body("User is not authenticated.");
     }
 }
