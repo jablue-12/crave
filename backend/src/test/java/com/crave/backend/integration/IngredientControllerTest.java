4a259cda331573b4d440254452f2bddece21376e
Jared Castillo <59758243+jablue-12@users.noreply.github.com>
2023-11-16 23:08:05 -0600

    Create Integration tests
    - Add integration tests for ingredients
    
    #82

---
+++ backend/src/test/java/com/crave/backend/integration/IngredientControllerTest.java
@@ -0,0 +1,69 @@
+package com.crave.backend.integration;
+
+import com.crave.backend.model.Ingredient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IngredientControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testGetIngredients() {
+        String url = "http://localhost:" + port + "/ingredients";
+
+        ResponseEntity<List> response = restTemplate.getForEntity(url, List.class);
+
+        assertEquals(OK, response.getStatusCode());
+        assertEquals(2, response.getBody().size()); // DB has 2 ingredients by default. Look at CraveConfig
+    }
+
+    @Test
+    public void testCreateIngredient() {
+        String url = "http://localhost:" + port + "/ingredients/create";
+
+        Ingredient ingredient = Ingredient.builder()
+                .name("TestIngredient")
+                .tag("TestTag")
+                .quantity(1.0)
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+
+        HttpEntity<Ingredient> requestEntity = new HttpEntity<>(ingredient, headers);
+
+        ResponseEntity<Ingredient> response = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                requestEntity,
+                Ingredient.class
+        );
+
+        assertEquals(CREATED, response.getStatusCode());
+        assertEquals(ingredient.getName(), response.getBody().getName());
+        assertEquals(ingredient.getTag(), response.getBody().getTag());
+        assertEquals(ingredient.getQuantity(), response.getBody().getQuantity());
+
+        // Cleanup: Delete the created ingredient if necessary
+        restTemplate.delete("http://localhost:" + port + "/ingredients/" + response.getBody().getId());
+    }
+}
---
+++ backend/src/test/java/com/crave/backend/integration/IngredientServiceTest.java
@@ -0,0 +1,47 @@
+package com.crave.backend.integration;
+
+import com.crave.backend.model.Ingredient;
+import com.crave.backend.repository.IngredientRepository;
+import com.crave.backend.service.IngredientService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
+public class IngredientServiceTest {
+
+    @Autowired
+    private IngredientService ingredientService;
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    @Test
+    public void testGetIngredients() {
+        List<Ingredient> ingredients = ingredientService.getIngredients();
+
+        // Has 2 ingredients based on the craveConfig (Fake data also applies to test db)
+        assertEquals(2, ingredients.size());
+    }
+
+    @Test
+    public void testCreateIngredient() {
+        Ingredient ingredient = Ingredient.builder()
+                .name("TestIngredient")
+                .tag("TestTag")
+                .quantity(1.0)
+                .build();
+
+        Ingredient createdIngredient = ingredientService.createIngredient(ingredient);
+
+        assertEquals(ingredient.getName(), createdIngredient.getName());
+        assertEquals(ingredient.getTag(), createdIngredient.getTag());
+        assertEquals(ingredient.getQuantity(), createdIngredient.getQuantity());
+    }
+}
